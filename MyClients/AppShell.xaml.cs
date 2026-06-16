ae2ba23c049b22cbe5c4f671ac81ea46366d0240
Artem-neloshkaPerevtilennia <bandura.artem@lll.kpi.ua>
2026-06-17 00:38:51 +0300

    Initial commit: clean .NET 10 MAUI setup

---
+++ MyClients/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyClients;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MyClients/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MyClients;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
