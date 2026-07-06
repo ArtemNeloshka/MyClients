c08c559386ca08e09b4c8d3fc323b99e4a3ffd54
Artem-neloshkaPerevtilennia <bandura.artem@lll.kpi.ua>
2026-07-06 03:01:11 +0300

    test data

--- MyClients/MauiProgram.cs
+++ MyClients/MauiProgram.cs
@@ -63,6 +63,7 @@ public static class MauiProgram
 	
 		var app = builder.Build();
 		
+		// test data
 		using (var scope = app.Services.CreateScope())
 		{
 		    var db = scope.ServiceProvider.GetRequiredService<MyClientsDbContext>();
