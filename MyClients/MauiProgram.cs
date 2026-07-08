0404e47d77dc89f6b92b6d9d74cf60da097288a0
Artem-neloshkaPerevtilennia <bandura.artem@lll.kpi.ua>
2026-07-08 22:06:07 +0300

    feat: training page add attempt button

--- MyClients/MauiProgram.cs
+++ MyClients/MauiProgram.cs
@@ -2,6 +2,7 @@
 using MyClients.BLL.Interfaces;
 using MyClients.BLL.Services;
 using MyClients.DAL;
+using MyClients.DAL.Entities;
 using MyClients.DAL.Repositories;
 using MyClients.ViewModels;
 using MyClients.Views;
@@ -59,6 +60,121 @@ public static class MauiProgram
 		builder.Services.AddSingleton<AppShell>();
 	
 		var app = builder.Build();
+		
+		// test data
+		Task.Run(async () =>
+		{
+			using (var scope = app.Services.CreateScope())
+			{
+				var db = scope.ServiceProvider.GetRequiredService<MyClientsDbContext>();
+				await db.Database.EnsureDeletedAsync();
+				await db.Database.EnsureDeletedAsync();
+
+				if (!db.Grades.Any())
+				{
+					db.Grades.AddRange(
+						new Grade { Name = "5a", Value = 1 },
+						new Grade { Name = "5b", Value = 2 },
+						new Grade { Name = "5c", Value = 3 },
+						new Grade { Name = "6a", Value = 4 },
+						new Grade { Name = "6a+", Value = 5 },
+						new Grade { Name = "6b", Value = 6 },
+						new Grade { Name = "6b+", Value = 7 },
+						new Grade { Name = "6c", Value = 8 },
+						new Grade { Name = "6c+", Value = 9 },
+						new Grade { Name = "7a", Value = 10 },
+						new Grade { Name = "7a+", Value = 11 },
+						new Grade { Name = "7b", Value = 12 }
+					);
+
+					await db.Disciplines.AddRangeAsync(
+						new Discipline
+						{
+							Name = "Bouldering",
+							Description =
+								"Bouldering is a form of free climbing performed on small rock formations or artificial rock walls without ropes or harnesses."
+						},
+						new Discipline
+						{
+							Name = "Top Rope Climbing",
+							Description =
+								"Top rope climbing is a style of climbing where the rope runs from a belayer up through an anchor at the top of the route and back down to the climber."
+						},
+						new Discipline
+						{
+							Name = "Lead Climbing",
+							Description =
+								"Lead climbing is a technique where the climber clips the rope to protection points as they ascend the route."
+						},
+						new Discipline
+						{
+							Name = "Speed Climbing",
+							Description =
+								"Speed climbing is a racing discipline where climbers race head-to-head on a standardized 15-meter wall."
+						}
+					);
+
+					await db.SaveChangesAsync();
+
+					var user = new User
+					{
+						Name = "Artem",
+						Surname = "Test",
+						Email = "artem@gmail.com",
+						Birthday = new DateOnly(2000, 1, 1),
+						PasswordHash = BCrypt.Net.BCrypt.HashPassword("12345678")
+					};
+					await db.Users.AddAsync(user);
+					await db.SaveChangesAsync();
+
+					var bouldering = db.Disciplines.First(d => d.Name == "Bouldering");
+					var topRope = db.Disciplines.First(d => d.Name == "Top Rope Climbing");
+					var lead = db.Disciplines.First(d => d.Name == "Lead Climbing");
+
+					var grade7a = db.Grades.First(g => g.Name == "7a");
+					var grade6b = db.Grades.First(g => g.Name == "6b");
+					var grade6c = db.Grades.First(g => g.Name == "6c+");
+
+					db.Trainings.AddRange(
+						new Training
+						{
+							UserId = user.Id, TrainingDate = new DateOnly(2025, 6, 1),
+							TrainingLog = "Good session, felt strong on the overhang."
+						},
+						new Training
+						{
+							UserId = user.Id, TrainingDate = new DateOnly(2025, 6, 3),
+							TrainingLog = "Worked on footwork and balance."
+						},
+						new Training
+						{
+							UserId = user.Id, TrainingDate = new DateOnly(2025, 6, 5),
+							TrainingLog = "Tough day, arms were pumped."
+						}
+					);
+
+					db.PersonalRecords.AddRange(
+						new PersonalRecord
+						{
+							UserId = user.Id, DisciplineId = bouldering.Id, GradeId = grade7a.Id,
+							RecordDate = new DateOnly(2025, 3, 10)
+						},
+						new PersonalRecord
+						{
+							UserId = user.Id, DisciplineId = topRope.Id, GradeId = grade6c.Id,
+							RecordDate = new DateOnly(2025, 4, 15)
+						},
+						new PersonalRecord
+						{
+							UserId = user.Id, DisciplineId = lead.Id, GradeId = grade6b.Id,
+							RecordDate = new DateOnly(2025, 5, 20)
+						}
+					);
+
+					await db.SaveChangesAsync();
+				}
+			}
+		});
 		
 		return app;
 	}
--- MyClients/Views/TrainPage.xaml.cs
+++ MyClients/Views/TrainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls.Shapes;
 
 namespace MyClients.Views;
 
@@ -21,5 +22,104 @@ public partial class TrainPage : ContentPage
 	private void OnFinishTrainingClicked(object? sender, TappedEventArgs e)
 	{
 		throw new NotImplementedException();
+	}
+
+	private void OnAddAttemptClicked(object? sender, EventArgs e)
+	{
+		var border = new Border
+		{
+			StrokeShape = new RoundRectangle { CornerRadius = 14 },
+			BackgroundColor = Color.FromArgb("#80D9D9D9"),
+			Padding = 5,
+		};
+		
+		var grid = new Grid
+		{
+			RowDefinitions =
+			{
+				new RowDefinition { Height = GridLength.Star },
+				new RowDefinition { Height = GridLength.Star }
+			},
+			ColumnDefinitions =
+			{
+				new ColumnDefinition { Width = GridLength.Star },
+				new ColumnDefinition { Width = GridLength.Star }
+			},
+			RowSpacing = 5,
+			ColumnSpacing = 10,
+		};
+
+		var disciplinePickerBorder = new Border
+		{
+			StrokeShape = new RoundRectangle { CornerRadius = 14 },
+			BackgroundColor = Color.FromArgb("#D9D9D9"),
+			Padding = 0,
+		};
+
+		var disciplinePicker = new Picker
+		{
+			Title = "Discipline",
+			HorizontalTextAlignment = TextAlignment.Center,
+			VerticalTextAlignment = TextAlignment.Center,
+			FontAttributes = FontAttributes.Bold | FontAttributes.Italic,
+			FontSize = 12,
+			ItemsSource = new List<string> { "Bouldering", "Top Rope", "Lead", "Speed" },
+		};
+
+		var gradePickerBorder = new Border
+		{
+			StrokeShape = new RoundRectangle { CornerRadius = 14 },
+			BackgroundColor = Color.FromArgb("#D9D9D9"),
+			Padding = 0,
+		};
+		
+		var gradePicker = new Picker
+		{
+			Title = "Grade",
+			HorizontalTextAlignment = TextAlignment.Center,
+			VerticalTextAlignment = TextAlignment.Center,
+			FontAttributes = FontAttributes.Bold | FontAttributes.Italic,
+			FontSize = 12,
+			ItemsSource = new List<string> { "5", "6", "7", "8" },
+		};
+
+		var climbResultPickerBorder = new Border
+		{
+			StrokeShape = new RoundRectangle { CornerRadius = 14 },
+			BackgroundColor = Color.FromArgb("#D9D9D9"),
+			Padding = 0,
+		};
+		
+		var climbResultPicker = new Picker
+		{
+			Title = "Result",
+			HorizontalTextAlignment = TextAlignment.Center,
+			VerticalTextAlignment = TextAlignment.Center,
+			FontAttributes = FontAttributes.Bold | FontAttributes.Italic,
+			FontSize = 12,
+			ItemsSource = new List<string> { "Flash", "Top", "Zone", "Failed" },
+		};
+		
+		Grid.SetRow(disciplinePickerBorder, 0);
+		Grid.SetColumn(disciplinePickerBorder, 0);
+		Grid.SetColumnSpan(disciplinePickerBorder, 2);
+		
+		Grid.SetRow(gradePickerBorder, 1);
+		Grid.SetColumn(gradePickerBorder, 0);
+		
+		Grid.SetRow(climbResultPickerBorder, 1);
+		Grid.SetColumn(climbResultPickerBorder, 1);
+
+		disciplinePickerBorder.Content = disciplinePicker;
+		gradePickerBorder.Content = gradePicker;
+		climbResultPickerBorder.Content = climbResultPicker;
+		
+		grid.Children.Add(disciplinePickerBorder);
+		grid.Children.Add(gradePickerBorder);
+		grid.Children.Add(climbResultPickerBorder);
+		
+		border.Content = grid;
+		
+		TrainingAttemptsContainer.Children.Add(border);
 	}
 }
