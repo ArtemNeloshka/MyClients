f55cef351e4845e8cd8bdeba60e29e1fbb9a6e99
Artem-neloshkaPerevtilennia <bandura.artem@lll.kpi.ua>
2026-07-19 16:11:38 +0300

    feat: training details page demo

--- MyClients/Views/TrainingDetailsPage.xaml.cs
+++ MyClients/Views/TrainingDetailsPage.xaml.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyClients.ViewModels;
 
 namespace MyClients.Views;
 
 public partial class TrainingDetailsPage : ContentPage
 {
+	public int TrainingId { get; set; }
 	public TrainingDetailsPage()
 	{
 		InitializeComponent();
+	}
+
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		var viewModel = IPlatformApplication.Current.Services.GetService<TrainingDetailsViewModel>();
+		BindingContext = viewModel;
+		await viewModel.LoadTrainingAsync(TrainingId);
 	}
 }
